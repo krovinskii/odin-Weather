aacba6a63890ed1e4f8b65be498499b5524f44d9
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-01-23 14:20:51 -0600

    Notice of bug

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import { eventListeners } from "./eventListeners";
 // Initialize event listeners
 const listeners = eventListeners();
 listeners.setupEventListeners();
+
+//Bug - if there is a space in the first item in the location array, it is removed. spaces and white space should not be removed from first item.
